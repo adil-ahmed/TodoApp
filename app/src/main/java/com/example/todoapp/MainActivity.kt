3e329de745b0701316618ef68076d315b11c88a0
adil-ahmed <adil.aj95@gmail.com>
2023-07-16 23:04:20 -0400

    Bottom sheet added

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.todoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindings: ActivityMainBinding
+    private lateinit var taskViewModel: TaskViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindings = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindings.root)
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        bindings.newTaskButton.setOnClickListener{
+            NewTaskSheet().show(supportFragmentManager, "newTaskTag")
+        }
+        taskViewModel.name.observe(this){
+            bindings.taskName.text = String.format("Task Name: %s", it)
+        }
+
+        taskViewModel.desc.observe(this){
+            bindings.taskDesc.text = String.format("Task Desc: %s", it)
+        }
     }
 }
---
+++ app/src/main/java/com/example/todoapp/NewTaskSheet.kt
@@ -0,0 +1,48 @@
+package com.example.todoapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.todoapp.databinding.ActivityMainBinding
+import com.example.todoapp.databinding.FragmentNewTaskSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class NewTaskSheet : BottomSheetDialogFragment() {
+    private lateinit var binding: FragmentNewTaskSheetBinding
+    private lateinit var taskViewModel: TaskViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val activity = requireActivity()
+        taskViewModel = ViewModelProvider(activity).get(TaskViewModel::class.java)
+        binding.saveButton.setOnClickListener {
+            saveAction()
+        }
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentNewTaskSheetBinding.inflate(inflater,container,false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    private fun saveAction()
+    {
+        taskViewModel.name.value = binding.name.text.toString()
+        taskViewModel.desc.value = binding.desc.text.toString()
+        binding.name.setText("")
+        binding.desc.setText("")
+        dismiss()
+
+
+    }
+}
