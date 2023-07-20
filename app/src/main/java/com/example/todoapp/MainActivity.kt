7c05fe57853f944fa06939aacb119eb7ece7e9a8
adil-ahmed <adil.aj95@gmail.com>
2023-07-19 23:00:24 -0400

    Recyclerview implemented

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.todoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,14 +15,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(bindings.root)
         taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
         bindings.newTaskButton.setOnClickListener{
-            NewTaskSheet().show(supportFragmentManager, "newTaskTag")
+            NewTaskSheet(null).show(supportFragmentManager, "newTaskTag")
         }
-        taskViewModel.name.observe(this){
-            bindings.taskName.text = String.format("Task Name: %s", it)
-        }
+        setRecyclerView()
+//        taskViewModel.name.observe(this){
+//            bindings.taskName.text = String.format("Task Name: %s", it)
+//        }
+//
+//        taskViewModel.desc.observe(this){
+//            bindings.taskDesc.text = String.format("Task Desc: %s", it)
+//        }
+    }
 
-        taskViewModel.desc.observe(this){
-            bindings.taskDesc.text = String.format("Task Desc: %s", it)
+    private fun setRecyclerView() {
+        val mainActivity = this
+        taskViewModel.taskItems.observe(this){
+            bindings.todoListRecyclerView.apply {
+                layoutManager = LinearLayoutManager(applicationContext)
+                adapter = TaskItemAdapter(it)
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/todoapp/TaskItem.kt
@@ -0,0 +1,14 @@
+package com.example.todoapp
+
+import java.time.LocalDate
+import java.time.LocalTime
+import java.util.UUID
+
+class TaskItem(
+    var name: String,
+    var desc: String,
+    var dueTime: LocalTime?,
+    var completedDate: LocalDate?,
+    var id: UUID = UUID.randomUUID()
+) {
+}
